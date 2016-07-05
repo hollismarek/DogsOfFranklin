b3f95188323bf68526f1ad640bb9cc2bd697eae1
hollismarek <hmarek@cafemarek.com>
2016-07-04 23:10:21 -0400

    Adding user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :animal_bios
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index', as: 'home_index'
@@ -6,4 +7,12 @@ Rails.application.routes.draw do
   get 'home_gallery', to: 'home#gallery'
   get 'home_contact', to: 'home#contact'
   get 'home_links', to: 'home#links'
+
+  get 'signup', to: 'users#new'
+  resources :users
+
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
